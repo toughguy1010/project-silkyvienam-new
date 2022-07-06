7ae9de8de08c073d69622917a557a4d86952b691
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2022-07-06 15:43:22 +0700

    commit -m "update about us"

--- wp-content/themes/silky-new/page-about.php
+++ wp-content/themes/silky-new/page-about.php
@@ -2,12 +2,36 @@
 
   do_action('about_style')
    ?>
-  
-  <!-- Content start -->
-  
-  <!-- Content end -->
+  <header class="about-header entry-header">
+	<?php
+                do_action('head');
+            ?>
+<?php // the_title( '<h1 class="entry-title about-title">', '</h1>' ); ?>
 
+	</header><!-- .entry-header -->
 
+	<?php // echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="about-section entry-content">
+		
+	
+		<?php
+		the_content();
+		understrap_link_pages();
+		?>
+		
+		<div class="entry-meta">
+
+			<?php // understrap_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
 
         <?php 
  get_footer();
