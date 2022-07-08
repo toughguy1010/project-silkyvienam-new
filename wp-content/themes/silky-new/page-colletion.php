6c1c3512d6bcfe0fd1007a6b10aefd9981354655
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2022-07-08 17:53:34 +0700

    commit -m "update collection"

--- wp-content/themes/silky-new/page-colletion.php
+++ wp-content/themes/silky-new/page-colletion.php
@@ -2,13 +2,22 @@
 get_header();
 do_action('product_style');	
 
+define("COLLECTION_PAGE_SIZE", 3);
 // print the IMG HTML
 $parent_id = 24;
 // parent category ID
 $parentterm = get_term_by( 'id', $parent_id, 'product_cat' ) ;
 // child category ID
 
-$termchildren = get_terms('product_cat',array('child_of' => $parent_id));
+// $termchildren = get_terms('product_cat',array('child_of' => $parent_id));
+
+$page = ( get_query_var('paged') ) ? get_query_var( 'paged' ) : 1;
+$taxonomy = 'product_cat';
+$offset = ( $page-1 ) * COLLECTION_PAGE_SIZE;
+$args = array( 'number' => COLLECTION_PAGE_SIZE, 'offset' => $offset, 'child_of' => $parent_id );
+$termchildren = get_terms( $taxonomy, $args );
+
+var_dump($termchildren);
 
 ?>
 
@@ -56,19 +65,53 @@ $termchildren = get_terms('product_cat',array('child_of' => $parent_id));
     </div>
     <div class="colletion-pagination">
         <?php 
-            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-            $args = array(
-                'post_type'=> 'post',
-                'orderby'    => 'ID',
-                'post_status' => 'publish',
-                'order'    => 'DESC',
-                'posts_per_page' => 2,// this will retrive all the post that is published 
-                'paged' => $paged
-                );
-                $result = new WP_Query( $args );
+        global $wp_query;
+        $cat = $wp_query->get_queried_object();
+
+        // get the query object
+        // var_dump($cat);
+        // var_dump( $cat);
+    
+        $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+        $args = array(
+            'post_type'             => 'product',
+            'post_status'           => 'publish',
+            'ignore_sticky_posts'   => 1,
+            'posts_per_page'        => '2',
+            'paged' => $paged,
+            'tax_query'             => array(
+                array(
+                    'taxonomy'      => 'product_cat',
+                    'field' => 'term_id', //This is optional, as it defaults to 'term_id'
+                    'terms'         => $cat->term_id,
+                    'operator'      => 'IN' // Possible values are 'IN', 'NOT IN', 'AND'.
+                ),
+                array(
+                    'taxonomy'      => 'product_visibility',
+                    'field'         => 'slug',
+                    'terms'         => 'exclude-from-catalog', // Possibly 'exclude-from-search' too
+                    'operator'      => 'NOT IN'
+                )
+            )
+        );
+
+        $termchildren = get_terms('product_cat',array('child_of' => $parent_id));
+        //     // $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+        //     // $args = array(
+        //     //     'post_type'=> 'post',
+        //     //     'orderby'    => 'ID',
+        //     //     'post_status' => 'publish',
+        //     //     'order'    => 'DESC',
+        //     //     'posts_per_page' => 2,// this will retrive all the post that is published 
+        //     //     'paged' => $paged
+        //     //     );
+        //         $result = new WP_Query( $args );
+        // $cat = $wp_query->get_queried_object($args);
+        // var_dump( $cat);
+  
                 echo paginate_links( array(
                     'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
-                    'total'        => $result->max_num_pages,
+                    'total'        => ceil(count($termchildren) /COLLECTION_PAGE_SIZE),
                     'current'      => max( 1, get_query_var( 'paged' ) ),
                     'format'       => '?paged=%#%',
                     'show_all'     => false,
@@ -81,7 +124,8 @@ $termchildren = get_terms('product_cat',array('child_of' => $parent_id));
                     'add_args'     => false,
                     'add_fragment' => '',
                 ) );
-        ?>
+            
+        // ?>
     </div>
 </div>
 
--- wp-content/themes/silky-new/woocommerce/content-product.php
+++ wp-content/themes/silky-new/woocommerce/content-product.php
@@ -49,28 +49,33 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	// do_action( 'woocommerce_shop_loop_item_title' );
 	?> 
 	<div class="product-desc">
-	<div class="product-title"><?php echo apply_filters( 'silky_filter-product-title_name', get_the_title() ); ?></div><?php
-	global $product;
+	<div class="product-title"><?php echo apply_filters( 'silky_filter-product-title_name', get_the_title() ); ?>
+	<div class="collection-product-short-desc">
+		<?php
+		global $product;
 	
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	// do_action( 'woocommerce_after_shop_loop_item_title' );
+		/**
+		 * Hook: woocommerce_after_shop_loop_item_title.
+		 *
+		 * @hooked woocommerce_template_loop_rating - 5
+		 * @hooked woocommerce_template_loop_price - 10
+		 */
+		// do_action( 'woocommerce_after_shop_loop_item_title' );
+		
+		$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
+		
+		if ( ! $short_description ) {
+			return;
+		}
+		$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
+		echo $short_description;
+		?>
+	</div>
+
+	</div>
 	
-$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
+	<?php
 
-if ( ! $short_description ) {
-	return;
-}
-
-?>
-<div class="woocommerce-product-details__short-description detail_sub_title">
-	<?php echo $short_description; // WPCS: XSS ok. ?>
-</div>
-<?
 	?> <span class="product-price"><?php echo apply_filters('silky_filter-product-price',$product->get_price_html() ) ?></span> 
 	</div>
 	<?php
